4302d86831d204f58dfdf18c2af60a145a3f3d90
Mike Piccolo <mfpiccolo@gmail.com>
2014-11-24 23:11:16 -0800

    Configured database cleaner

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,10 +7,6 @@ require "minitest/autorun"
 require 'capybara/rails'
 require 'capybara/dsl'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[File.join("./test/support/**/*.rb")].sort.each { |f| require f }
-
 Capybara.javascript_driver = :webkit
 Capybara.current_driver = Capybara.javascript_driver
 
@@ -22,8 +18,12 @@ class IntegrationHelper < MiniTest::Spec
   end
 
   after do
-    DatabaseCleaner.clean
     Capybara.reset_sessions!
+    DatabaseCleaner.clean
   end
 end
 MiniTest::Spec.register_spec_type( /Integration$/, IntegrationHelper )
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[File.join("./test/support/**/*.rb")].sort.each { |f| require f }
