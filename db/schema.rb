92a4420f38664c58d1923736ec240c271d9acb59
Mike Piccolo <mfpiccolo@gmail.com>
2014-12-11 00:50:35 -0800

    Add priority to liine_items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141208093528) do
+ActiveRecord::Schema.define(version: 20141211073904) do
 
   create_table "invoices", force: true do |t|
     t.binary   "uuid",           limit: 16
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20141208093528) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "complete"
+    t.string   "priority"
   end
 
 end
